30ab262e6f116ac5f5f2c6815753537d5825e3c2
KonradMarzec1991 <33575891+KonradMarzec1991@users.noreply.github.com>
2026-01-19 20:23:49 +0100

    Add Profile

--- lynx/models.py
+++ lynx/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(null=True, blank=True, default='')
+    user = models.ForeignKey(User, max_length=10, on_delete=models.CASCADE, null=True)
