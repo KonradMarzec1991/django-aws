4c5db404f91a8338a76faa886b153560a19a1ed5
KonradMarzec1991 <33575891+KonradMarzec1991@users.noreply.github.com>
2026-01-19 20:30:31 +0100

    Allow image uploads

--- lynx/models.py
+++ lynx/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    profile_pic = models.ImageField(null=True, blank=True, default='')
+    profile_pic = models.ImageField(null=True, blank=True, default='Default.png')
     user = models.ForeignKey(User, max_length=10, on_delete=models.CASCADE, null=True)
